78bf0460e1e426fb9ac358c54f8cce43c8d337a5
EC2 Default User <ec2-user@ip-172-31-84-149.ec2.internal>
2022-07-03 11:09:26 +0000

    [Fix error]deviseのサインアウトエラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,9 +29,10 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to books_path
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   devise_for :users
   root to: 'homes#top'
-  get 'home/about' => 'homes#about', as: 'about'
-  resources :books
-  resources :users, only: [:index, :show, :edit]
   patch 'users/:id' => 'users#update', as: 'update_users'
   delete 'books/:id' => 'books#destroy', as: 'delete_book'
+  get 'home/about' => 'homes#about', as: 'about'
+  resources :users, only: [:index, :show, :edit]
+  resources :books
+  delete 'users/sign_out' => "session#destroy", as: 'delete_user_session'
   # resources :homes, :only => [:about]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
