d1e34d238d5f8a73113e906f97f5f7e55b6f2f92
EC2 Default User <ec2-user@ip-172-31-84-149.ec2.internal>
2022-07-05 05:05:15 +0000

    [Add]いいね機能のためのコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
   devise_for :users
   resources :users, only: [:index, :show, :edit]
   resources :books
